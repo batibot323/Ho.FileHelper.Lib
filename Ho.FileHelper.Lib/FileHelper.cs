8a2a2b655d94dd7d80a57e4b9174b032ed72c8e7
batibot323 <hani.herbert.ho@gmail.com>
2019-12-28 03:54:29 +0800

    Added SetOutputToFile() and CloseOutputStream() to FileHelper.

--- Ho.FileHelper.Lib/FileHelper.cs
+++ Ho.FileHelper.Lib/FileHelper.cs
@@ -8,6 +8,10 @@ namespace Ho.FileHelper.Lib
 {
     public static class FileHelper
     {
+        public static FileStream ostrm;
+        public static StreamWriter writer;
+        public static TextWriter oldOut;
+
         public static string SolutionFolder()
         {
             DirectoryInfo directory = new DirectoryInfo(Directory.GetCurrentDirectory());
@@ -17,17 +21,39 @@ namespace Ho.FileHelper.Lib
             }
             return directory.FullName.ToString();
         }
-
         public static string AppendDateTime(string fileName)
         {
             string appendedFileName = $"{fileName}{ DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") }.txt";
             return appendedFileName;
         }
-
         public static string GenerateFilePath(string folderName, string fileName)
         {
             string generatedFilePath = Path.Combine(SolutionFolder(), folderName, AppendDateTime(fileName));
             return generatedFilePath;
+        }
+        public static void SetOutputToFile(string folderName, string fileName)
+        {
+            oldOut = Console.Out;
+            try
+            {
+                ostrm = new FileStream(GenerateFilePath(folderName, fileName),
+                    FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open {0} for writing", fileName);
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.SetOut(writer);
+        }
+        public static void CloseOutputStream()
+        {
+            Console.SetOut(oldOut);
+            writer.Close();
+            ostrm.Close();
+            Console.WriteLine("Done");
         }
     }
 }
